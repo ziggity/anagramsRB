bf3bb9772b75703f922cd2e8c25a85c2538fa8eb
zach beecher <zachbeecher@gmail.com>
2017-04-21 11:14:09 -0700

    2nd test passing for comparing two single letter a is not b, false outcome

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,5 +5,9 @@ describe('String#anagram') do
 	it('This will return true for the anagram "Z" of the single letter word "Z"') do
 		expect('Z'.anagram('Z')).to(eq(['Z', true]))
 	end
+  it('returns false for the anagram "Z" of the single letter word "Y"') do
+  		expect('Y'.anagram('Z')).to(eq(['Z', false]))
+  	end
+
 
 end
