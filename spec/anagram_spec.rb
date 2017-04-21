a3f730f7a675338fed883ed5a986e6fc9f2f71ae
zach beecher <zachbeecher@gmail.com>
2017-04-21 11:21:15 -0700

    pass test for one word anagram that is not true, ie tacocat is not anagram of robot

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,11 +5,13 @@ describe('String#anagram') do
 	it('This will return true for the anagram "Z" of the single letter word "Z"') do
 		expect('Z'.anagram('Z')).to(eq(['Z', true]))
 	end
-  it('returns false for the anagram "Z" of the single letter word "Y"') do
+  it('This returns false for the anagram "Z" of the single letter word "Y"') do
   		expect('Y'.anagram('Z')).to(eq(['Z', false]))
   end
   it('This returns true for a one word anagram "lasvegas" of the word "salvages"') do
 		expect('salvages'.anagram('lasvegas')).to(eq(['lasvegas', true]))
 	end
-
+  it('This returns false for a one word anagram "tacocat" of the word "robot"') do
+		expect('robot'.anagram('tacocat')).to(eq(['tacocat', false]))
+	end
 end
