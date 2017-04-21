b8398ff70dfbc7158439b5fbe75e832abd6fe3c7
zach beecher <zachbeecher@gmail.com>
2017-04-21 10:59:18 -0700

    test passing for single letter anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,20 @@
 class String
-  define_method(:anagram) do
+	define_method(:anagram) do |words|
+		user_input = self.split("").sort()
+		user_anagram = words.split("").sort()
+		anagram_array = []
+		if user_input.==(user_anagram)
+			anagram_array.push(words, true)
+		else
+			anagram_array.push(words, false)
+		end
+	end
+end
 
-end
-end
+
+#Zach, don't forget to include the array of anagrams->:0
+# if list_of_anagrams.include? user_input
+#   puts "Oh goody! that's a 100% genuine anagram you got there!"
+# else
+#   puts "Better luck next time pal, this one is not an anagram, it's more like an antigram!"
+# end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,6 +2,8 @@ require('rspec')
 require('./lib/anagram.rb')
 
 describe('String#anagram') do
-  it('returns anagrams') do
-    expect(('tacocat').anagram()).to(eq('tacocat'))
-  end
+	it('This will return true for the anagram "Z" of the single letter word "Z"') do
+		expect('Z'.anagram('Z')).to(eq(['Z', true]))
+	end
+
+end
