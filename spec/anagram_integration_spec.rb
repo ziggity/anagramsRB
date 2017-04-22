94735a17afccdd377b4b76012ace8f120b6c1fdf
zach beecher <zachbeecher@gmail.com>
2017-04-21 17:05:32 -0700

    added integration test

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -3,11 +3,12 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('anagram checking process', {:type => :feature}) do
-#   it('processes the user entry of text inputs, and checks if they are anagrams') do
-#     visit('/')
-#     fill_in('anagram', :with => 'tacocat')
-#     click_button('Anagramatic go!')
-#     expect(page).to have_content('tacocat')
-#   end
-# end
+describe('anagram checking process', {:type => :feature}) do
+  it('processes the user entry of text inputs, and checks if they are anagrams') do
+    visit('/')
+    fill_in('phrase1', :with => 'tacocat')
+    fill_in('phrase2', :with => 'tacocat')
+    click_button('Anagramatic go!')
+    expect(page).to have_content('tacocat')
+  end
+end
